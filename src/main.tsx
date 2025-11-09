a9cb3ba22ccd8c9b1526a418597a2ac76d505497
klaro-health <stephan.buttgereit@gmail.com>
2025-11-09 09:28:53 +0100

    main.tsx aktualisieren

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/tokens.css'   //  ⟵ neu
+import './styles/table.css'     // ⟵ neu
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
